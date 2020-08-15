13aa3be83f7a3428994ff8f32708bf211a409f14
Jim Waschura <jim.waschura@gmail.com>
2020-08-15 14:54:39 -0700

    initial creation

---
+++ Python/pyBitwiseAutomation/__init__.py
@@ -0,0 +1,11 @@
+
+from .autogenAccessory import *
+from .autogenCommon import *
+from .autogenPega import *
+from .AutomationExtender import *
+from .AutomationInterface import *
+from .SocketDevice import *
+from .BitwiseDevice import *
+from .PegaDevice import *
+
+
